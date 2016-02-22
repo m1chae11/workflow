c0eeecb62db57bb29abbad7b2506bd9f2f6f5059
michael <mike@creativeatwork.net>
2016-02-23 12:49:12 +1300

    new changed to gulpjs

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,17 @@
 var gulp = require('gulp'),
-	gutil = require('gulp-util');
+	gutil = require('gulp-util'),
+	coffee = require('gulp-coffee');
 
 // command :gulp log
 gulp.task('log',function(){
 	gutil.log('testing awesomnes of gulp');
 })	
+var coffeeSources = ['components/coffee/tagline.coffee'];
+gulp.task('coffee',function(){
+	gulp.src(coffeeSources)
+	// compile javascript without safety wrapper!
+	.pipe(coffee({bare:true})
+		.on('error', gutil.log))
+	.pipe(gulp.dest('components/scripts'))
+	
+})
