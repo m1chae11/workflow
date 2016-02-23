429bcffed6f962e380c22ea1fc98de9edcc8fb14
michael <mike@creativeatwork.net>
2016-02-23 13:14:04 +1300

    added gulp-concat plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,28 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
-	coffee = require('gulp-coffee');
+	coffee = require('gulp-coffee'),
+	concat = require('gulp-concat');
 
 // command :gulp log
 gulp.task('log',function(){
 	gutil.log('testing awesomnes of gulp');
 })	
+
 var coffeeSources = ['components/coffee/tagline.coffee'];
+var jsSources = ['components/scripts/*.js'];
+
 gulp.task('coffee',function(){
 	gulp.src(coffeeSources)
 	// compile javascript without safety wrapper!
 	.pipe(coffee({bare:true})
 		.on('error', gutil.log))
-	.pipe(gulp.dest('components/scripts'))
-	
+	.pipe(gulp.dest('components/scripts'))	
+})
+
+gulp.task('js', function(){
+	gulp.src(jsSources)
+		.pipe(concat('scripts.js')
+			.on('error',gutil.log))
+		.pipe(gulp.dest('builds/developments/js'))
+
 })
