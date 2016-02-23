1f47a18117265e3efed50f43e218f3108d1aa2e4
michael <mike@creativeatwork.net>
2016-02-23 15:33:05 +1300

    added compass

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	coffee = require('gulp-coffee'),
-	concat = require('gulp-concat');
+	concat = require('gulp-concat'),
+	browserify = require('gulp-browserify'),
+	compass = require('gulp-compass');
 
 // command :gulp log
 gulp.task('log',function(){
 	gutil.log('testing awesomnes of gulp');
 })	
-
+var sassSources = ['components/sass/style.scss']
 var coffeeSources = ['components/coffee/tagline.coffee'];
 var jsSources = ['components/scripts/*.js'];
 
@@ -23,6 +25,19 @@ gulp.task('js', function(){
 	gulp.src(jsSources)
 		.pipe(concat('scripts.js')
 			.on('error',gutil.log))
+		.pipe(browserify())
 		.pipe(gulp.dest('builds/developments/js'))
+
+})
+
+gulp.task('compass',function(){
+	gulp.src(sassSources)
+	.pipe(compass({
+		sass:'components/sass',
+		image:'builds/developments/images',
+		style:'expanded'
+	})
+		.on('error',gutil.log))
+	.pipe(gulp.dest('builds/developments/css'))
 
 })
